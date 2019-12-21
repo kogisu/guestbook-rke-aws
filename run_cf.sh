65046ae6f78fe6372b7e70716698ed5dd00958cb
Kent Ogisu <kentogisu@gmail.com>
2019-12-21 17:12:24 -0500

    Allowed all incoming / outgoing ports to instances

--- run_cf.sh
+++ run_cf.sh
@@ -24,7 +24,8 @@ update_stack() {
   aws cloudformation update-stack \
   --stack-name $1 \
   --template-body file://$2 \
-  --parameters file://$3
+  --parameters file://$3 \
+  --capabilities CAPABILITY_IAM
   return 0
 }
 
